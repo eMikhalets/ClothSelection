12322fae864bc09df7953a81fd1d72abd06d4dd3
Mikhalets Evgeny <evg.mikhalets@gmail.com>
2021-12-17 22:08:52 +0300

    init

---
+++ src/main/java/com/emikhalets/clothselection/service/ClientService.java
@@ -0,0 +1,18 @@
+package com.emikhalets.clothselection.service;
+
+import com.emikhalets.clothselection.model.Client;
+
+import java.util.List;
+
+public interface ClientService {
+
+    void create(Client client);
+
+    List<Client> readAll();
+
+    Client read(int id);
+
+    boolean update(Client client, int id);
+
+    boolean delete(int id);
+}
---
+++ src/test/java/com/emikhalets/clothselection/ClothselectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emikhalets.clothselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothselectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
